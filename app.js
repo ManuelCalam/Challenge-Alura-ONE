1ae17ce632339a2786c8814a5f640c14c32c614f
ManuelCalam <jmanuelc369@gmail.com>
2025-08-04 23:23:10 -0600

    Add friend function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriend(name) {
+    let newFriend = document.querySelector('#amigo').value;
+    if (newFriend === '') {
+        alert('Por favor, ingresa un nombre.');
+    } else if (friends.includes(newFriend)) {
+        alert('Este amigo ya está en la lista.');
+    } else {
+        friends.push(newFriend);
+        let friendList = document.querySelector('.name-list');
+        let listItem = document.createElement('li');
+        listItem.textContent = newFriend;
+        friendList.appendChild(listItem);
+        document.querySelector('#amigo').value = ''; 
+    }
+}
